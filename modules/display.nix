910ae78e337e6755886e26aa7770912db0dab5df
kn100 <kn100@kn100.me>
2024-05-25 02:16:22 -0400

    whateveR

--- modules/display.nix
+++ modules/display.nix
@@ -6,7 +6,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.systemPackages = with pkgs; [gnomeExtensions.appindicator gnome.adwaita-icon-theme];
+  environment.systemPackages = with pkgs; [gnomeExtensions.appindicator gnome.adwaita-icon-theme pulseview];
   services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
 
   # Ask electron apps to not be shit and use Wayland
